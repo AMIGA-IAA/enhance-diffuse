af94409406bc9e16e6f2b5ca8ed0a2edacdbe8d6
jmoldon <jmoldon@iaa:.es>
2019-10-03 13:34:55 +0200

    Find fits files in Inputs folder

--- enhance_diffuse.py
+++ enhance_diffuse.py
@@ -1,6 +1,9 @@
+import os
 import configparser
+import glob
 
 config_file = './params.cfg'
+input_location = './Inputs'
 
 def read_config(config_file, print_values=False):
     """
@@ -16,8 +19,18 @@ def read_config(config_file, print_values=False):
             print(f'{key:12} : {value}')
     return config
 
+def find_image_names(input_location):
+    location = os.path.join(input_location, '*fits')
+    print(f'Looking for files in {location}')
+    files = glob.glob(location)
+    if len(files) > 0:
+        print('Files to process {}'.format(files))
+    else:
+        print('Warning, no files found')
+
 def main():
     config = read_config(config_file, print_values=True)
+    find_image_names(input_location)
 
 if __name__=="__main__":
     main()
